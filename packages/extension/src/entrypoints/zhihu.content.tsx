95c021639c18b857c03153616ea55894c5744247
summer <summer_88488@hotmail.com>
2026-04-05 23:48:31 +0800

    chore: 限制浏览器插件仅在指定知乎页面运行

--- packages/extension/src/entrypoints/zhihu.content.tsx
+++ packages/extension/src/entrypoints/zhihu.content.tsx
@@ -2,7 +2,7 @@ import { defineContentScript } from 'wxt/utils/define-content-script';
 
 // Keep runtime code inside `main` (WXT imports entrypoints in Node during build).
 export default defineContentScript({
-  matches: ['https://*.zhihu.com/*'],
+  matches: ['https://zhuanlan.zhihu.com/p/*', 'https://www.zhihu.com/question/*'],
   runAt: 'document_idle',
 
   async main() {
